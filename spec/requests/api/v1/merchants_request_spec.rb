6fe9b61bf895d085944dda205be87da67623def8
Jamie Pace <jamiejpace@gmail.com>
2021-10-23 11:27:01 -0600

    Add tests for get all merchants endpoint, create serializer, create controller and index action

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,11 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchants API' do
-  it 'sends a list of 20 students at a time' do
-    create_list(:merchant, 21)
+  it 'returns a list of 20 students at a time as default' do
+    create_list(:merchant, 22)
 
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants).to be_a(Hash)
+
+    expect(merchants).to have_key(:data)
+    expect(merchants[:data]).to be_an(Array)
+
+    merchants[:data].each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_a(String)
+
+      expect(merchant).to have_key(:type)
+      expect(merchant[:type]).to be_a(String)
+
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
+    end
+  end
+
+  it 'returns a list of 20 students given a page number parameter' do
+    create_list(:merchant, 22)
+
+    get '/api/v1/merchants', params: { page: 2 }
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants[:data].count).to eq(2)
+  end
+
+  it 'can return a different number of students per page' do
+    create_list(:merchant, 30)
+
+    get '/api/v1/merchants', params: { per_page: 25 }
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants[:data].count).to eq(25)
+  end
+
+  it 'returns an empty array if there are no merchants' do
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants).to have_key(:data)
+    expect(merchants[:data]).to eq([])
   end
 end

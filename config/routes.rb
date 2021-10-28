e0c6ec282e230599dbeeb269e6e68ae194e87a11
Jamie Pace <jamiejpace@gmail.com>
2021-10-28 11:37:56 -0600

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,13 @@ Rails.application.routes.draw do
       get '/revenue/merchants', to: 'revenue/merchants#ranked_by_revenue'
       get '/revenue/merchants/:id', to: 'revenue/merchants#total_revenue'
       get '/revenue/items', to: 'revenue/items#ranked_by_revenue'
+
       get '/merchants/most_items', to: 'merchants#merchant_most_items_sold'
 
       get '/merchants/find', to: 'merchants#find'
       get '/items/find_all', to: 'items#find_all'
 
-      resources :merchants, only: [:index, :show]
-
-      resources :merchants, only: [:show] do
+      resources :merchants, only: [:index, :show] do
         resources :items, only: [:index]
       end
 
